4d0e03a5c81ee51bc2199bc44e8bf51f18c2e273
Alex Baker <hello@alexbaker.io>
2020-06-07 17:48:17 -0600

    Add inital extensions list

---
+++ extensions.sh
@@ -0,0 +1,8 @@
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension mgmcdermott.vscode-language-babel
+code --install-extension ms-python.python
+code --install-extension ms-vscode.Go
+code --install-extension percygrunwald.vscode-intellij-recent-files
+code --install-extension techer.open-in-browser
+code --install-extension vscodevim.vim
